88e908383452582bf9d2f77a8e2d305a89d55974
John Meredith <johnmeredith@users.noreply.github.com>
2018-09-12 12:25:22 +0100

    First commit

---
+++ src/pages/404.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Layout from '../components/layout'
+
+const NotFoundPage = () => (
+  <Layout>
+    <div id="main" className="alt">
+        <section id="one">
+            <div className="inner">
+                <h1>INTEROPERABILITY FAILURE</h1>
+                <p>All forms of technical, semantic and process interop failed... the sadness.</p>
+            </div>
+        </section>
+    </div>
+  </Layout>
+)
+
+export default NotFoundPage
