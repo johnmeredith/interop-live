ea25b55c3a118f233931d415c99195daa3325440
John Meredith <johnmeredith@users.noreply.github.com>
2018-10-12 19:57:47 +0100

    Index images column tweak. Register page details

--- src/pages/register.js
+++ src/pages/register.js
@@ -19,7 +19,12 @@ const Register = (props) => (
                         <h1>Register To Attend</h1>
                     </header>
                     <span className="image main"><img src={theatre_chairs} alt="" /></span>
+                    <p>Attendance at the 2019 Health Interoperability Symposium is FREE b ut places are limited.</p>
+                    <h2>Target Audience</h2>
+                    <p>The symposium is aimed at anyone who works in the field of health interoperability. The events aims to have a frank and honest discussion about the challenges the NHS faces and will be of interest to CIOs, CCIOs, CMIO’s, SME decision-makers, software architects, developers, leaders of change and innovators.</p>
+                    <p>Click the following link to register:</p>
                     <a href="https://www.eventbrite.co.uk/e/health-interoperability-symposium-2019-registration-51001806777?ref=ebtnebregn" target="_blank"><img src="https://www.eventbrite.co.uk/custombutton?eid=51001806777" alt="Eventbrite - Health Interoperability Symposium 2019" /></a>
+                
                 </div>    
             </section>
         </div>
