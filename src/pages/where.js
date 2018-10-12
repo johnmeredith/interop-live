8e74353e8d05a1b7173add7c15273c8fd3644321
John Meredith <johnmeredith@users.noreply.github.com>
2018-10-12 19:23:02 +0100

    Updated address and Cardiff Image

--- src/pages/where.js
+++ src/pages/where.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Helmet from 'react-helmet'
 import Layout from '../components/layout'
 
-import cardiff_bay from '../assets/images/cardiff_bay.jpg'
+import cardiff_bay from '../assets/images/cardiff_bay_wide.jpg'
 import icon_train from '../assets/images/icon_train.png'
 import icon_car from '../assets/images/icon_car.png'
 import icon_plane from '../assets/images/icon_plane.png'
@@ -21,7 +21,7 @@ const Location = (props) => (
                     <h1>Come to Interop 2019</h1>
                     </header>
                     <span className="image main"><img src={cardiff_bay} alt="" /></span>
-                    <p>The Interoperability Symposium 2019 with be held in Cardiff, Wales. </p>
+                    <p>The Interoperability Symposium 2019 with be held in Cardiff, Wales at NHS Wales Informatics Service </p>
                     <p>Cardiff, Wales’ capital city offers spectacular shopping, museums, dining and entertainment. Easy to explore on foot you can visit the free  National Museum Cardiff and spectacular Cardiff Castle all within the city centre.</p> 
                     <p>Visit the official regional website for more information at <a href="http://www.visitcardiff.com">Visit Cardiff</a>.</p>
                     <h2>How to get here</h2>
@@ -31,6 +31,8 @@ const Location = (props) => (
                     <p><span className="image left"><img src={icon_train} alt="Travel by train" /></span>Cardiff has direct rail links to many cities in the UK including London, Birmingham, Manchester, Nottingham, Bristol and Southampton. The journey time from London Paddington to Cardiff is just over 2 hours and leaves every 30 minutes.</p>
                     <h3>Air</h3>
                     <p><span className="image left"><img src={icon_plane} alt="Travel by plane" /></span>Cardiff Airport is 12 miles west of the city centre and 10 miles from Junction 33 of the M4. The airport has direct flights to over 50 destinations and connecting flights to over 800 destinations worldwide. Visit the Cardiff Wales airport website for details. Airlines operating from Cardiff include KLM, Aer Lingus, Flybe, Vueling and Ryanair.</p>
+                    <span>Ty Glan Yr Afon<br>21 Cowbridge Road East<br>Cardiff CF11 9AD<br>Wales</span>
+    
     </div>
             </section>
         </div>
