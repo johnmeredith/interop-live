194d09bd92496e0c98a9ab96f2e713d4add63d80
John Meredith <johnmeredith@users.noreply.github.com>
2018-10-12 17:50:04 +0100

    Revert "Merge branch 'master' of https://github.com/johnmeredith/interop-live"
    This reverts commit 2e72d908b96cc934fb1da1c6db2b59d6ccfad8a6, reversing
    changes made to 73a64cd89b47e058ae68e77b3faf624f41b1e4ea.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,8 +5,7 @@ const Contact = (props) => (
         <div className="inner">
             <section>
                 <h2>Contact Interop 2019</h2>
-                <form name="contact_interop19" method="post" action="/success" data-netlify="true" data-netlify-honeypot="bot-field">
-                    <input type="hidden" name="bot-field" />
+                <form name="contact_interop19" method="post" action="/success" data-netlify="true">
                     <div className="field half first">
                         <label htmlFor="name">Name</label>
                         <input type="text" name="name" id="name" />
