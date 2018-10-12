1d1d9039614c92618edfa74937f276b3aec91e40
John Meredith <johnmeredith@users.noreply.github.com>
2018-10-12 19:03:35 +0100

    Update src/pages/index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,19 +4,12 @@ import Helmet from 'react-helmet'
 import Layout from '../components/layout'
 import Banner from '../components/Banner'
 
-import pic01 from '../assets/images/pic01.jpg'
-import pic02 from '../assets/images/pic02.jpg'
-import pic03 from '../assets/images/pic03.jpg'
-import pic04 from '../assets/images/pic04.jpg'
-import pic05 from '../assets/images/pic05.jpg'
-import pic06 from '../assets/images/pic06.jpg'
 import cardiff_bay from '../assets/images/cardiff_bay.jpg'
 import theatre_chairs from '../assets/images/theatre_chairs.jpg'
-import screen_code from '../assets/images/screen_code.jpg'
-import neon_lights from '../assets/images/neon_lights.jpg'
+import question_trees from '../assets/images/evan-dennis-75563-unsplash.jpg'
 import speaker from '../assets/images/oleg-laptev-546607-unsplash.jpg'
-import logo_uwtsd from '../assets/images/uwtsd.gif'
-import logo_nhs_wales from '../assets/images/NHS_Wales_70_Logo.jpg'
+import logo_uwtsd from '../assets/images/uwtsd-logo.jpg'
+import logo_nwis from '../assets/images/nwis-logo.jpg'
 
 
 class HomeIndex extends React.Component {
@@ -37,12 +30,12 @@ class HomeIndex extends React.Component {
 
                 <div id="main">
                     <section id="one" className="tiles">
-                        <article style={{backgroundImage: `url(${neon_lights})`}}>
+                        <article style={{backgroundImage: `url(${question_trees})`}}>
                             <header className="major">
                                 <h3>Why</h3>
                                 <p>The conversation about interoperability by the people that know it best.</p>
                             </header>
-                            <Link to="/landing" className="link primary"></Link>
+                            <Link to="/why" className="link primary"></Link>
                         </article>
                         <article style={{backgroundImage: `url(${theatre_chairs})`}}>
                             <header className="major">
@@ -56,7 +49,7 @@ class HomeIndex extends React.Component {
                                 <h3>Speakers</h3>
                                 <p>Meet our speakers!</p>
                             </header>
-                            <Link to="/landing" className="link primary"></Link>
+                            <Link to="/speakers" className="link primary"></Link>
                         </article>
                         <article style={{backgroundImage: `url(${cardiff_bay})`}}>
                             <header className="major">
@@ -73,7 +66,7 @@ class HomeIndex extends React.Component {
                             </header>
                             <div className="box alt">
                                     <div className="grid-wrapper">
-                                        <div className="col-4"><span className="image fit"><img src={logo_nhs_wales} alt="NHS Wales" /></span></div>
+                                        <div className="col-4"><span className="image fit"><img src={logo_nwis} alt="NHS Wales Informatics Service" /></span></div>
                                         <div className="col-4"><span className="image fit"><img src={logo_uwtsd} alt="University of Wales Trinity St Davids" /></span></div>
                                     </div>
                                 </div>
@@ -83,12 +76,12 @@ class HomeIndex extends React.Component {
                         </div>
                     </section>
                     <section id="three">
-                        <div className="inner">
+                    <div className="inner">
                             <header className="major">
-                                <h2>This is another section.</h2>
+                                <h2>Register for Interop'19</h2>
                             </header>
                             <div className="box alt">
-                            <p>Some text</p>
+                            <p>Interop'19 is <bold>FREE TO ATTEND.</bold></p>
                          </div>
                             <ul className="actions">
                                 <li><Link to="/register" className="button next">Register Now</Link></li>
