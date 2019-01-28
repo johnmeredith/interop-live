2fbceaa6b8091c0e422d9c2ac13ad936e350f173
John Meredith <johnmeredith@users.noreply.github.com>
2019-01-28 22:53:06 +0000

    Update news.js

--- src/pages/news.js
+++ src/pages/news.js
@@ -20,10 +20,11 @@ const News = (props) => (
                     <header className="major">
                         <h3>28/01/19 - Interop'19 Gets An Upgrade</h3>
                     </header>
-                    <p><span className="image right"><img src={ccfc} alt="Cardiff City Stadium" /></span>
+                    <p><span className="image right"><img src={dhew} alt="Digital Health Ecosystem Wales" /></span>
+                        
                     <strong>Interop'19 is on the move to Cardiff City Stadium.</strong></p>
                     <p>Due to the great demand for attendance, we are pleased to announce that the Interoperability Symposium is moving to Cardiff City Stadium. We are hoping to bring you a bigger event, with more collaboration thanks to our friends at the Digital Health Ecosystem Wales.</p>
-                    <p><span className="image right"><img src={dhew} alt="Digital Health Ecosystem Wales" /></span>Digital Health Ecosystem Wales is a network connecting developers and companies with innovative digital health solutions with the NHS in Wales. It represents a collaboration between the Life Sciences Hub Wales and the NHS Wales Informatics Service (NWIS) and is funded through the Welsh Government’s Efficiency Through Technology Programme.</p>
+                    <p><span className="image right"><img src={ccfc} alt="Cardiff City Stadium" /></span>Digital Health Ecosystem Wales is a network connecting developers and companies with innovative digital health solutions with the NHS in Wales. It represents a collaboration between the Life Sciences Hub Wales and the NHS Wales Informatics Service (NWIS) and is funded through the Welsh Government’s Efficiency Through Technology Programme.</p>
                     <p>There is no change to the date of the event and the larger <a href="../register">ticket allocation is available now.</a></p>
                 </div>
             </section>
