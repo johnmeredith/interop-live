3f6510007986a4511c4bc234d7231fb268ad3079
John Meredith <johnmeredith@users.noreply.github.com>
2019-01-28 22:49:43 +0000

    Update news.js
    More tweaks

--- src/pages/news.js
+++ src/pages/news.js
@@ -17,24 +17,23 @@ const News = (props) => (
         <div id="main" className="alt">
             <section id="one">
                 <div className="inner">
-                    <header className="major"><span className="image right"><img src={ccfc} alt="Cardiff City Stadium" /></span>
+                    <header className="major">
                         <h3>28/01/19 - Interop'19 Gets An Upgrade</h3>
                     </header>
-                    <p>
+                    <p><span className="image right"><img src={ccfc} alt="Cardiff City Stadium" /></span>
                     <strong>Interop'19 is on the move to Cardiff City Stadium.</strong></p>
                     <p>Due to the great demand for attendance, we are pleased to announce that the Interoperability Symposium is moving to Cardiff City Stadium. We are hoping to bring you a bigger event, with more collaboration thanks to our friends at the Digital Health Ecosystem Wales.</p>
-                    <p><span className="image right"><img src={bevan} alt="The Bevan Commission" /></span>Digital Health Ecosystem Wales is a network connecting developers and companies with innovative digital health solutions with the NHS in Wales. It represents a collaboration between the Life Sciences Hub Wales and the NHS Wales Informatics Service (NWIS) and is funded through the Welsh Government’s Efficiency Through Technology Programme.</p>
+                    <p><span className="image right"><img src={dhew} alt="Digital Health Ecosystem Wales" /></span>Digital Health Ecosystem Wales is a network connecting developers and companies with innovative digital health solutions with the NHS in Wales. It represents a collaboration between the Life Sciences Hub Wales and the NHS Wales Informatics Service (NWIS) and is funded through the Welsh Government’s Efficiency Through Technology Programme.</p>
                     <p>There is no change to the date of the event and the larger <a href="../register">ticket allocation is available now.</a></p>
                 </div>
             </section>
             <section id="two">
                 <div className="inner">
                 <header className="major">
-                    <span className="image right"><img src={bevan} alt="The Bevan Commission" /></span>
-                    <h3>23/01/19 - Collaboration Update</h3>
+                                        <h3>23/01/19 - Collaboration Update</h3>
                     </header>
-                            <p>
-                            <strong>We are very pleased to announce that we are collaborating with @BevanCommission and Apperta Foundation on #Interop19!</strong>
+                            <p><span className="image right"><img src={bevan} alt="The Bevan Commission" /></span>
+                            <strong>We are very pleased to announce that Interop'19 is collaborating with the Bevan Commission and Apperta Foundation.</strong>
                             </p>
                             <p>
                                 <a href="http://www.bevancommission.org/en/">The Bevan Commission</a> is a group of international experts providing advice to the Minister for Health and Social Services and ensuring that Wales can draw on best healthcare practices from around the world while remaining true to the principles of the NHS as established by Aneurin Bevan.
