3dad3a2f9133637acde4903c414bc292bbc39a0f
John Meredith <johnmeredith@users.noreply.github.com>
2019-03-05 15:19:50 +0000

    Update agenda.js

--- src/pages/agenda.js
+++ src/pages/agenda.js
@@ -22,7 +22,60 @@ const Agenda = (props) => (
                         <p>The programme of speakers promises to be informative, enlightening and thought provoking. The aim of the Interoperability Symposium is to provide a forum to discuss what interoperability means in 2019 for the NHS. </p>
                         <p>The speakers all have a story to tell and experience to share from across the UK and Europe.</p>
                     </div>
-                    <h3>We will be publishing the full symposium agenda shortly.</h3>
+                    <h1>Symposium Agenda</h1>
+                                <div className="table-wrapper">
+                                    <table className="alt">
+                                        <thead>
+                                            <tr>
+                                                <th>Time</th>
+                                                <th>Agenda Item / Talk</th>
+                                                <th>Presenter</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td>09:00</td>
+                                                <td>Coffee</td>
+                                                <td></td>
+                                            </tr>
+                                            <tr>
+                                                <td>09:20</td>
+                                                <td>Welcome and Opening Remarks</td>
+                                                <td>Andrew Griffiths, NHS Wales Informatics Service</td>
+                                            </tr>
+                                            <tr>
+                                                <td>09:45</td>
+                                                <td><strong>Session 1</strong></td>
+                                                <td>Sion Charles, Bevan Commission</td>
+                                            </tr>
+                                            <tr>
+                                                <td></td>
+                                                <td>Clinical Interoperability</td>
+                                                <td>Professor John Williams, Swansea University</td>
+                                            </tr>
+                                            <tr>
+                                                <td></td>
+                                                <td>Getting the Basics Right</td>
+                                                <td>Tim Benson, R-Outcomes Ltd</td>
+                                            </tr>
+                                            <tr>
+                                                <td></td>
+                                                <td>HiGHmed - An Open Platform Approach</td>
+                                                <td>Birger Haarbrandt, HiGHmed Project</td>
+                                            </tr>
+                                        </tbody>
+                                        <tfoot>
+                                            <tr>
+                                                <td colSpan="2"></td>
+                                                <td>100.00</td>
+                                            </tr>
+                                        </tfoot>
+                                    </table>
+</div>
+
+
+
+
                 </div>
             </section>
         </div>
