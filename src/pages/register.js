17d5e9ea3eff5413544135f4ce4e2a3b5b364dc7
John Meredith <johnmeredith@users.noreply.github.com>
2019-03-05 15:24:25 +0000

    Update register.js
    Reversed eventbrite form

--- src/pages/register.js
+++ src/pages/register.js
@@ -26,10 +26,7 @@ const Register = (props) => (
                     <p>The symposium is aimed at anyone who works in the field of health interoperability. The events aims to have a frank and honest discussion about the challenges the NHS faces and will be of interest to CIOs, CCIOs, CMIO’s, SME decision-makers, software architects, developers, leaders of change and innovators.</p>
                     <p>Click the following link to register:</p>
                     <a href="https://www.eventbrite.co.uk/e/health-interoperability-symposium-2019-registration-51001806777?ref=ebtnebregn" target="_blank"><img src="https://www.eventbrite.co.uk/custombutton?eid=51001806777" alt="Eventbrite - Health Interoperability Symposium 2019" /></a>
-   
-                </div>
-    <iframe src="https://eventbrite.co.uk/tickets-external?eid=51001806777&ref=etckt" frameborder="0" height="308" width="100%" vspace="0" hspace="0" marginheight="5" marginwidth="5" scrolling="auto" allowtransparency="true"></iframe><div style="font-family:Helvetica, Arial; font-size:12px; padding:10px 0 5px; margin:2px; width:100%; text-align:left;" ><a class="powered-by-eb" style="color: #ADB0B6; text-decoration: none;" target="_blank" href="https://www.eventbrite.co.uk/">Powered by Eventbrite</a></div></div>
- 
+                </div>    
             </section>
         </div>
 
